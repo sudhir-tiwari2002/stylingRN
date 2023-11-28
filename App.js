d6e747fb01298aeac3328648f07401ccd46c9379
sudhir.pw <sudhir.tiwari@pw.live>
2023-11-28 14:49:10 +0530

    styling using stylesheet API

--- App.js
+++ App.js
@@ -3,18 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Text style={style.title}>StyleSheet API</Text>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style =StyleSheet.create({
+  container:{flex:1,backgroundColor:"plum",padding:60},
+  title : {color:'green'}
+})
